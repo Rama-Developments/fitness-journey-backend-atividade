4753874ca402f17168735213cc5825816b711062
Steven Reis <steven.oliveira@ciss.com.br>
2024-12-19 11:08:19 -0300

    fix: cors allow all

--- src/main/java/fitness/journey/backend/atividade/RunAtividade.java
+++ src/main/java/fitness/journey/backend/atividade/RunAtividade.java
@@ -2,6 +2,10 @@ package fitness.journey.backend.atividade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class RunAtividade {
@@ -9,5 +13,12 @@ public class RunAtividade {
     public static void main(String[] args) {
 
         SpringApplication.run(RunAtividade.class, args);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
