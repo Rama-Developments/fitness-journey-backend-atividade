79d188b2886cffd591bf1be64bc9e9a2fa917704
Steven Reis <steven.oliveira@ciss.com.br>
2024-12-19 10:19:16 -0300

    feat: adicionado caso de uso findOneAtividade

---
+++ src/main/java/fitness/journey/backend/atividade/domain/application/usecases/FindOneAtividade.java
@@ -0,0 +1,13 @@
+package fitness.journey.backend.atividade.domain.application.usecases;
+
+import fitness.journey.backend.atividade.domain.enterprise.entities.Atividade;
+import fitness.journey.backend.atividade.shared.annotations.UseCase;
+import fitness.journey.backend.atividade.shared.usecases.AbstractFindOneUseCase;
+
+/**
+ * @author stevenreis
+ * @since 1.0 (19/12/24)
+ */
+@UseCase
+public class FindOneAtividade extends AbstractFindOneUseCase<Atividade> {
+}
